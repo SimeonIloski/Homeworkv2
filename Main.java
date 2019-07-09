d3ea0a7c368a1dfa0ef24da71d35e62b2669d22f
SimeonIloski <simeoniloski@gmail.com>
2019-07-09 09:49:37 +0200

    secondBranch first commit

--- Main.java
+++ Main.java
@@ -7,6 +7,8 @@ public class Main {
         System.out.println("third change");
         System.out.println("firstbranch first change");
         System.out.println("firstbranch second change");
+        System.out.println("secondbranch first change");
+
 
     }
 }
