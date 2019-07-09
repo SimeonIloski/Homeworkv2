820d61d6eecff5878ad42659c7248db3252edefd
SimeonIloski <simeoniloski@gmail.com>
2019-07-09 09:13:09 +0200

    second change on master

--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("first change")
+        System.out.println("second change")
+
+
     }
 }
