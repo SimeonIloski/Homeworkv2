c4565ebff399a664737ffac9b30004285d3d785f
SimeonIloski <simeoniloski@gmail.com>
2019-07-09 09:41:13 +0200

    firstBranch first commit

--- Main.java
+++ Main.java
@@ -2,10 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("first change")
-        System.out.println("second change")
-        System.out.println("third change")
-
+        System.out.println("first change");
+        System.out.println("second change");
+        System.out.println("third change");
+        System.out.println("firstbranch first change");
 
     }
 }
